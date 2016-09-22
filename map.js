f8c2a4165f24bb8e55869822c975c14d248da5cd
Vitalii Denysiuk <vitalii_denysiuk@ukr.net>
2016-09-22 22:16:08 +0300

    fix indentation

--- map.js
+++ map.js
@@ -1,15 +1,15 @@
 function map(object, fun) {
-	let arr = [];
-	let newObject = {};
-	 if(Array.isArray(object)) {
+ let arr = [];
+ let newObject = {};
+  if(Array.isArray(object)) {
    for(let i = 0; i < object.length; i++)
-	 		arr[i] = fun(object[i]);
-	 	return arr;
-	 } else {
-	 		for (let key in object) 
-   			newObject[key] = fun(object[key]);
-			return newObject;
-	 }
+    arr[i] = fun(object[i]);
+    return arr;
+  } else {
+      for (let key in object) 
+       newObject[key] = fun(object[key]);
+       return newObject;
+      }
 }
  
 console.log(map([1, 2, 3, 4], (el) => ++el));
