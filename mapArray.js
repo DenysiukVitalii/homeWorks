e5803bafd1138beb1e759d0b778f4d6c10a5ba4c
Vitalii Denysiuk <vitalii_denysiuk@ukr.net>
2016-09-22 22:17:12 +0300

    fix indentation

--- mapArray.js
+++ mapArray.js
@@ -1,10 +1,10 @@
 
 function mapArray(arr, fun) {
-	let newArr = [];
-	for (var i = 0; i < arr.length; i++) {
-		newArr[i] = fun(arr[i]);
-	}
-	return newArr;
+ let newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+   newArr[i] = fun(arr[i]);
+  }
+  return newArr;
 }
  
 console.log(mapArray([1, 2, 3, 4], (el) => ++el));
